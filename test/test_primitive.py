081dc268593e6aef38f23883e72e4269cb497ffd
Christian Höntsch-Rode <hoentsch.rode@gmail.com>
2021-08-05 19:01:57 +0200

    Housekeeping

--- test/test_primitive.py
+++
@@ -1,9 +0,0 @@
-"""Primitive test just to have something to discover."""
-
-import pytest
-
-
-def test_simple():
-    """Stupid test function."""
-    assert True
-    assert 0.891 == pytest.approx(1, 0.001)
